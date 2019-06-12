e0d75261cfc006a03efafadd9b12e41a6b55457e
Muthulekshmi-99 <51690021+Muthulekshmi-99@users.noreply.github.com>
2019-06-12 11:45:01 +0530

    Create vowel-consonanat.py

---
+++ code/vowel-consonanat.py
@@ -0,0 +1,10 @@
+z=input()
+y=['a','s','d','f','g','h','j','k','l','q','w','e','r','t','y','u','i','o','p','z','x','c','v','b','n','m']
+x=['a','e','i','o','u']
+if z in y:
+  if z in x:
+    print('Vowel')
+  else:
+    print('Consonant')
+else:
+  print('invalid')
